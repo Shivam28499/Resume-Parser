72607333fe66b315d1561d28bb3facaf5b853fa6
Shivam Kumar <168215301+Shivam28499@users.noreply.github.com>
2026-05-08 12:56:22 +0530

    Fixed cors and dotenv

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import resumeRoutes from './routes/resumeRoutes.js';
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT;
